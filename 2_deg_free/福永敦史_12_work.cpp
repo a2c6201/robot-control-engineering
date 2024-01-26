396922d40f82fbcbdcf43c65d2cff423339d852b
a2c6201 <a2c6201@gmail.com>
2024-01-26 18:35:46 +0900

    feat:calculateAngleを授業通りの解法に変更

--- 2_deg_free/福永敦史_12_work.cpp
+++ 2_deg_free/福永敦史_12_work.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <cmath>
 #include <fstream>
-#include <cmath>
 
 double pi = M_PI;
 
 // PD制御のパラメータ
+const double h = 0.001; // 刻み幅(s)
+const double t = 0.2;   // シミュレーション時間(s)
 const double Kp = 1.0; // 比例ゲイン
 const double Kd = 0.5; // 微分ゲイン
 
@@ -22,33 +23,26 @@ double targets[][2] = {
   {0.3, 0.3}  // t = 0.8
 };
 
-// ロボットアーム先端から逆運動学により関節角度を計算する関数
-void inverseKinematics(double x, double y, double& theta1, double& theta2) {
+// ロボットアーム先端から逆運動学により目標関節角度を計算する関数
+void calculateAngle(double x, double y, double& theta1, double& theta2) {
   double epsilon = 1e-6; // 許容誤差
 
   theta1 = pi/2.0;
   theta2 = -pi/2.0;
 
-  // 最大反復回数
-  int maxIterations = 100;
+  // 分割数
+  int N = t/h;
 
   // ヤコビ行列を使用して逆運動学を解く
-  for (int i = 0; i < maxIterations; i++) {
-    double r = sqrt(x*x + y*y);
-    double phi = atan2(y, x);
+  for (int i = 0; i < N; i++) {
 
-    // Forward kinematics to compute the end effector position
+    // 順運動学を使用してアーム先端位置を計算する
     double xEnd = L1 * cos(theta1) + L2 * cos(theta1 + theta2);
     double yEnd = L1 * sin(theta1) + L2 * sin(theta1 + theta2);
 
-    // Compute the error between the desired position and the end effector position
-    double errorX = x - xEnd;
-    double errorY = y - yEnd;
-
-    // 誤差が小さくなったらループを抜ける
-    if (fabs(errorX) < epsilon && fabs(errorY) < epsilon) {
-      break; // Solution found
-    }
+    // 目標位置とアーム先端位置の誤差を計算する
+    double deltaX = (x-xEnd) / (N-i);
+    double deltaY = (y-yEnd) / (N-i);
 
     // ヤコビ行列を計算する
     double J11 = -L1 * sin(theta1) - L2 * sin(theta1 + theta2);
@@ -58,19 +52,19 @@ void inverseKinematics(double x, double y, double& theta1, double& theta2) {
 
     // ヤコビ行列の逆行列を計算する
     double detJ = J11 * J22 - J12 * J21;
-    double invJ11 = J22 / detJ;
-    double invJ12 = -J12 / detJ;
-    double invJ21 = -J21 / detJ;
-    double invJ22 = J11 / detJ;
+    double A11 = J22 / detJ;
+    double A12 = -J12 / detJ;
+    double A21 = -J21 / detJ;
+    double A22 = J11 / detJ;
 
     // 逆ヤコビ行列を使用してtheta1とtheta2を更新する
-    theta1 += invJ11 * errorX + invJ12 * errorY;
-    theta2 += invJ21 * errorX + invJ22 * errorY;
+    theta1 += theta1 + A11*deltaX + A12*deltaY;
+    theta2 += theta2 - A21*deltaX - A22*deltaY;
   }
 }
 
 // PD制御を使用して関節トルクを計算する関数
-void calculateJointTorques(double theta1, double theta2, double& tau1, double& tau2, double prevTheta1, double prevTheta2) {
+void calculateTorques(double theta1, double theta2, double& tau1, double& tau2, double prevTheta1, double prevTheta2) {
   double error1 = theta1 - prevTheta1;
   double error2 = theta2 - prevTheta2;
 
@@ -94,10 +88,10 @@ int main() {
     double y = targets[i][1];
 
     double theta1, theta2;
-    inverseKinematics(x, y, theta1, theta2);
+    calculateAngle(x, y, theta1, theta2);
 
     double tau1, tau2;
-    calculateJointTorques(theta1, theta2, tau1, tau2, prevTheta1, prevTheta2);
+    calculateTorques(theta1, theta2, tau1, tau2, prevTheta1, prevTheta2);
 
     // 前回の関節角度を更新する
     prevTheta1 = theta1;
